d6280d81767af196849fd69bd29f077341a55d6a
FrantisekHalcin <116026496+FrantisekHalcin@users.noreply.github.com>
2022-11-25 01:23:34 +0100

    Add files via upload

---
+++ operacie.js
@@ -0,0 +1,12 @@
+
+function matematickeOperacie(x = 4, y = 2) {
+    
+    console.log("prvé číslo: " + x + "   druhé číslo: " + y);
+    console.log("Vykonávam matematické operácie");
+    console.log(x + " + " + y + " = " + (x+y));
+    console.log(x + " - " + y + " = " + (x-y));
+    console.log(x + " * " + y + " = " + (x*y));
+    console.log(x + " / " + y + " = " + (x/y));
+
+}
+    
